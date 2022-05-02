668daf85c987421827f4dc69cbdc267edb6b91b8
Taha Enes Aslantürk <enes.aslanturk@ug.bilkent.edu.tr>
2022-05-02 12:41:08 +0300

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Taha Enes Aslantürk on 2.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
